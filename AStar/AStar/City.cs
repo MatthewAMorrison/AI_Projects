565ee9d6a2d2031895be76139409a4a29dbce103
unknown <Jon@.(none)>
2011-02-14 23:53:20 -0500

    Started the project with Visual Studio.

---
+++ AStar/AStar/City.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AStar
+{
+    class City
+    {
+        //Attributes
+        public string Name { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        List<City> connections { get; set; }
+        bool prevVisited { get; set; }
+        City prevCity { get; set; }
+
+
+        //Methods
+
+        public bool isConnected(City city)
+        {
+            foreach (var c in this.connections)
+            {
+                if (c.Name == city.Name) { return true; }
+                else { return false; }
+            }
+
+            return false;
+        }
+
+        public bool isVisited()
+        {
+            return this.prevVisited;
+        }
+    }
+}
---
+++ AStar/AStar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+        }
+    }
+}
